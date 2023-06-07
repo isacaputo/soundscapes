6672a96161b50722b6ed47ef91f91ff9d027bd81
Isadora Caputo <isadora.caputo@gmail.com>
2023-06-07 12:44:08 +0200

    Correct buffer error and add slider to control tremolo effect

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -16,6 +16,7 @@ import Stack from "@mui/material/Stack";
 import Slider from "@mui/material/Slider";
 import VolumeDown from "@mui/icons-material/VolumeDown";
 import VolumeUp from "@mui/icons-material/VolumeUp";
+import WavesIcon from "@mui/icons-material/Waves";
 
 // notes = ["C", "D", "E", "F", "G", "A", "B"]
 // octaves = [3, 4, 5]
@@ -36,6 +37,190 @@ const reverb = new Tone.Reverb({
   decay: 1.5,
   wet: 1,
 }).toDestination();
+
+//Main Sequence Buffers
+const buffer1 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A0.mp3"
+);
+const buffer2 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C1.mp3"
+);
+const buffer3 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds1.mp3"
+);
+const buffer4 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs1.mp3"
+);
+const buffer5 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A1.mp3"
+);
+const buffer6 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C2.mp3"
+);
+const buffer7 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds2.mp3"
+);
+const buffer8 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs2.mp3"
+);
+const buffer9 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A2.mp3"
+);
+const buffer10 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C3.mp3"
+);
+const buffer11 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds3.mp3"
+);
+const buffer12 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs3.mp3"
+);
+const buffer13 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A3.mp3"
+);
+const buffer14 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C4.mp3"
+);
+const buffer15 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds4.mp3"
+);
+const buffer16 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs4.mp3"
+);
+const buffer17 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A4.mp3"
+);
+const buffer18 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C5.mp3"
+);
+const buffer19 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds5.mp3"
+);
+const buffer20 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs5.mp3"
+);
+const buffer21 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A5.mp3"
+);
+const buffer22 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C6.mp3"
+);
+const buffer23 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds6.mp3"
+);
+const buffer24 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs6.mp3"
+);
+const buffer25 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A6.mp3"
+);
+const buffer26 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C7.mp3"
+);
+const buffer27 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds7.mp3"
+);
+const buffer28 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs7.mp3"
+);
+const buffer29 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A7.mp3"
+);
+const buffer30 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C8.mp3"
+);
+
+//Background Sequence Buffers
+const buffer31 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A0.mp3"
+);
+const buffer32 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C1.mp3"
+);
+const buffer33 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds1.mp3"
+);
+const buffer34 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs1.mp3"
+);
+const buffer35 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A1.mp3"
+);
+const buffer36 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C2.mp3"
+);
+const buffer37 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds2.mp3"
+);
+const buffer38 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs2.mp3"
+);
+const buffer39 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A2.mp3"
+);
+const buffer40 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C3.mp3"
+);
+const buffer41 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds3.mp3"
+);
+const buffer42 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs3.mp3"
+);
+const buffer43 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A3.mp3"
+);
+const buffer44 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C4.mp3"
+);
+const buffer45 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds4.mp3"
+);
+const buffer46 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs4.mp3"
+);
+const buffer47 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A4.mp3"
+);
+const buffer48 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C5.mp3"
+);
+const buffer49 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds5.mp3"
+);
+const buffer50 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs5.mp3"
+);
+const buffer51 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A5.mp3"
+);
+const buffer52 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C6.mp3"
+);
+const buffer53 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds6.mp3"
+);
+const buffer54 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs6.mp3"
+);
+const buffer55 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A6.mp3"
+);
+const buffer56 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C7.mp3"
+);
+const buffer57 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Ds7.mp3"
+);
+const buffer58 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/Fs7.mp3"
+);
+const buffer59 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/A7.mp3"
+);
+const buffer60 = new Tone.Buffer(
+  "https://tonejs.github.io/audio/salamander/C8.mp3"
+);
 
 //Instruments
 // const piano = new Tone.Sampler({
@@ -120,7 +305,7 @@ export default function App() {
   const [play, setPlay] = useState(false);
   const [countTempo, setCountTempo] = useState(null);
   const intervalRef = useRef();
-  const [tremoloFrequency, setTremoloFrequency] = useState(1);
+  const [tremoloFrequency, setTremoloFrequency] = useState(0);
   const [backgroundVolume, setBackgroundVolume] = useState(-10);
   const [mainVolume, setMainVolume] = useState(-10);
   const [backgroundSequence, setBackgroundSequence] = useState(
@@ -152,50 +337,53 @@ export default function App() {
     const mainPiano = new Tone.Sampler({
       volume: mainVolume,
       urls: {
-        A0: "A0.mp3",
-        C1: "C1.mp3",
-        "D#1": "Ds1.mp3",
-        "F#1": "Fs1.mp3",
-        A1: "A1.mp3",
-        C2: "C2.mp3",
-        "D#2": "Ds2.mp3",
-        "F#2": "Fs2.mp3",
-        A2: "A2.mp3",
-        C3: "C3.mp3",
-        "D#3": "Ds3.mp3",
-        "F#3": "Fs3.mp3",
-        A3: "A3.mp3",
-        C4: "C4.mp3",
-        "D#4": "Ds4.mp3",
-        "F#4": "Fs4.mp3",
-        A4: "A4.mp3",
-        C5: "C5.mp3",
-        "D#5": "Ds5.mp3",
-        "F#5": "Fs5.mp3",
-        A5: "A5.mp3",
-        C6: "C6.mp3",
-        "D#6": "Ds6.mp3",
-        "F#6": "Fs6.mp3",
-        A6: "A6.mp3",
-        C7: "C7.mp3",
-        "D#7": "Ds7.mp3",
-        "F#7": "Fs7.mp3",
-        A7: "A7.mp3",
-        C8: "C8.mp3",
+        A0: buffer1,
+        C1: buffer2,
+        "D#1": buffer3,
+        "F#1": buffer4,
+        A1: buffer5,
+        C2: buffer6,
+        "D#2": buffer7,
+        "F#2": buffer8,
+        A2: buffer9,
+        C3: buffer10,
+        "D#3": buffer11,
+        "F#3": buffer12,
+        A3: buffer13,
+        C4: buffer14,
+        "D#4": buffer15,
+        "F#4": buffer16,
+        A4: buffer17,
+        C5: buffer18,
+        "D#5": buffer19,
+        "F#5": buffer20,
+        A5: buffer21,
+        C6: buffer22,
+        "D#6": buffer23,
+        "F#6": buffer24,
+        A6: buffer25,
+        C7: buffer26,
+        "D#7": buffer27,
+        "F#7": buffer28,
+        A7: buffer29,
+        C8: buffer30,
       },
-      baseUrl: "https://tonejs.github.io/audio/salamander/",
-    }).toDestination();
+    })
+      .connect(tremolo)
+      .toDestination();
 
     const mainBasicSynth = new Tone.Synth({
       volume: mainVolume,
-    }).toDestination();
+    })
+      .connect(tremolo)
+      .toDestination();
 
     return {
       piano: mainPiano,
       amSynth: mainAmSynth,
       basicSynth: mainBasicSynth,
     };
-  }, [mainVolume]);
+  }, [mainVolume, tremolo]);
 
   const backgroundSynths = useMemo(() => {
     const backAmSynth = new Tone.AMSynth({
@@ -207,50 +395,53 @@ export default function App() {
     const backPiano = new Tone.Sampler({
       volume: backgroundVolume,
       urls: {
-        A0: "A0.mp3",
-        C1: "C1.mp3",
-        "D#1": "Ds1.mp3",
-        "F#1": "Fs1.mp3",
-        A1: "A1.mp3",
-        C2: "C2.mp3",
-        "D#2": "Ds2.mp3",
-        "F#2": "Fs2.mp3",
-        A2: "A2.mp3",
-        C3: "C3.mp3",
-        "D#3": "Ds3.mp3",
-        "F#3": "Fs3.mp3",
-        A3: "A3.mp3",
-        C4: "C4.mp3",
-        "D#4": "Ds4.mp3",
-        "F#4": "Fs4.mp3",
-        A4: "A4.mp3",
-        C5: "C5.mp3",
-        "D#5": "Ds5.mp3",
-        "F#5": "Fs5.mp3",
-        A5: "A5.mp3",
-        C6: "C6.mp3",
-        "D#6": "Ds6.mp3",
-        "F#6": "Fs6.mp3",
-        A6: "A6.mp3",
-        C7: "C7.mp3",
-        "D#7": "Ds7.mp3",
-        "F#7": "Fs7.mp3",
-        A7: "A7.mp3",
-        C8: "C8.mp3",
+        A0: buffer31,
+        C1: buffer32,
+        "D#1": buffer33,
+        "F#1": buffer34,
+        A1: buffer35,
+        C2: buffer36,
+        "D#2": buffer37,
+        "F#2": buffer38,
+        A2: buffer39,
+        C3: buffer40,
+        "D#3": buffer41,
+        "F#3": buffer42,
+        A3: buffer43,
+        C4: buffer44,
+        "D#4": buffer45,
+        "F#4": buffer46,
+        A4: buffer47,
+        C5: buffer48,
+        "D#5": buffer49,
+        "F#5": buffer50,
+        A5: buffer51,
+        C6: buffer52,
+        "D#6": buffer53,
+        "F#6": buffer54,
+        A6: buffer55,
+        C7: buffer56,
+        "D#7": buffer57,
+        "F#7": buffer58,
+        A7: buffer59,
+        C8: buffer60,
       },
-      baseUrl: "https://tonejs.github.io/audio/salamander/",
-    }).toDestination();
+    })
+      .connect(tremolo)
+      .toDestination();
 
     const backBasicSynth = new Tone.Synth({
       volume: backgroundVolume,
-    }).toDestination();
+    })
+      .connect(tremolo)
+      .toDestination();
 
     return {
       piano: backPiano,
       amSynth: backAmSynth,
       basicSynth: backBasicSynth,
     };
-  }, [backgroundVolume]);
+  }, [backgroundVolume, tremolo]);
 
   const getNext4ndIndex = (index) => {
     return index % 4 === 0;
@@ -422,6 +613,11 @@ export default function App() {
     setBackgroundVolume(newValue);
   };
 
+  const handleTremoloFrequencyChange = (event, newValue) => {
+    console.log(newValue);
+    setTremoloFrequency(newValue);
+  };
+
   return (
     <div className="board">
       <img src={`/images/soundscapes-logo.png`} className="logo-image" />
@@ -576,17 +772,25 @@ export default function App() {
                   <VolumeUp />
                 </Stack>
               </Box>
-              <Box sx={{ width: "80px" }}>
-                <Slider
-                  aria-label="Temperature"
-                  defaultValue={30}
-                  // getAriaValueText={valuetext}
-                  valueLabelDisplay="auto"
-                  step={10}
-                  marks
-                  min={10}
-                  max={110}
-                />
+              <Box sx={{ width: "120px" }}>
+                <Stack
+                  spacing={1}
+                  direction="row"
+                  sx={{ mb: 1, mt: 1 }}
+                  alignItems="center"
+                >
+                  <WavesIcon sx={{ transform: "scale(0.8)" }} />
+                  <Slider
+                    aria-label="tremolo"
+                    value={tremoloFrequency}
+                    onChange={handleTremoloFrequencyChange}
+                    step={2}
+                    marks
+                    min={0}
+                    max={15}
+                  />
+                  <WavesIcon />
+                </Stack>
               </Box>
 
               <div className="play-stop-container">
